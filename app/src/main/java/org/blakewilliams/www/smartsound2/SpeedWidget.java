eb90a8c58320f424563f341c0529457aff35cc81
blakewilliams1 <blauwill@ucsc.edu>
2016-09-13 15:23:24 -0700

    Temp working widget that directs to app

--- app/src/main/java/org/blakewilliams/www/smartsound2/SpeedWidget.java
+++ app/src/main/java/org/blakewilliams/www/smartsound2/SpeedWidget.java
@@ -12,12 +12,21 @@ import android.widget.RemoteViews;
  * Implementation of App Widget functionality.
  */
 public class SpeedWidget extends AppWidgetProvider {
-    private boolean isRunning = false;
+	public static String YOUR_AWESOME_ACTION = "YourAwesomeAction";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
+	        Intent intent = new Intent(context, SpeedWidget.class);
+	        intent.setAction(YOUR_AWESOME_ACTION);
+	        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+	        // Get the layout for the App Widget and attach an on-click listener to the button
+	        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.speed_widget);
+	        views.setOnClickPendingIntent(R.id.widgetButton, pendingIntent);
+
+	        // Tell the AppWidgetManager to perform an update on the current App Widget
+	        appWidgetManager.updateAppWidget(appWidgetId, views);
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
@@ -37,26 +46,28 @@ public class SpeedWidget extends AppWidgetProvider {
             int appWidgetId) {
 
         Intent intent = new Intent(context, MainActivity.class);
-        intent.putExtra("Goal",context.getString(R.string.stopThread));
+	    intent.setAction(YOUR_AWESOME_ACTION);
+
+	    //intent.putExtra("Goal",context.getString(R.string.stopThread));
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
         // Get the layout for the App Widget and attach an on-click listener
         // to the button
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.speed_widget);
+	    //TODO: try using widget layout id next
         views.setOnClickPendingIntent(R.id.widgetButton, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
-    void toggleApp(){
-        if(!isRunning){
-            Log.i("Widget","Turn on Smart Sound thread");
-        }else{
-            Log.i("Widget","Turn off Smart Sound thread");
-        }
-        isRunning=!isRunning;
-    }
+	@Override
+	public void onReceive(Context context, Intent intent){
+		super.onReceive(context,intent);
+		if (intent.getAction().equals(YOUR_AWESOME_ACTION)) {
+			Log.i("Widget","Caught custom action");
+		}
+	}
 
 }
 
